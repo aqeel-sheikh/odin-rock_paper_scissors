ce8fabe0db13fe52ad3e8188138a9117f3fb5584
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-07-12 15:44:06 +0530

    Function invocation: PlayRound
    Function to play a single round and keep track of the score who won and lose

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function getHumanChoice() {
     return prompt("Enter One: Rock, Paper or Scissor!").toLowerCase()
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice){
+        console.log("Tie: You both choose " + humanChoice)
+    }
+    else if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissor") 
+        || (humanChoice === "scissor" && computerChoice === "rock") ){
+        
+        console.log(`You Lose: ${computerChoice} beats ${humanChoice}`)
+        computerScore += 1
+    }
+    else{
+        console.log(`You Win: ${humanChoice} beats ${computerChoice}`)
+        humanScore += 1
+    }
+}
