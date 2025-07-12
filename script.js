757f57e3f20811ccfd75a88ba40ed9cb6b972594
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-07-12 15:43:40 +0530

    Function Invocation: getComputerScore and getHumanScore

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissor"];
+    const randomChoice = Math.floor(Math.random() * choices.length);
+    return choices[randomChoice].toLowerCase()
+}
+
+function getHumanChoice() {
+    return prompt("Enter One: Rock, Paper or Scissor!").toLowerCase()
+}
+
