3f88a8136712d6c5c7533026e70e3aba2649ec8f
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-07-15 18:32:14 +0530

    Upgraded from Console based to Ui based
    Added buttons in html file and implemented the logic to work with event listenrs

--- script.js
+++ script.js
@@ -1,39 +1,65 @@
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissor"];
+    const choices = ["Rock", "Paper", "Scissors"];
     const randomChoice = Math.floor(Math.random() * choices.length);
     return choices[randomChoice].toLowerCase()
 }
 
-function getHumanChoice() {
-    return prompt("Enter One: Rock, Paper or Scissor!").toLowerCase()
+const rock = document.querySelector(".rock")
+const paper = document.querySelector(".paper")
+const scissors = document.querySelector(".scissors")
+
+const results = document.querySelector(".results")
+const currentResult = document.querySelector(".result")
+const finalResult = document.querySelector(".anouncement")
+
+const hScore = document.querySelector("#humanScore")
+const cScore = document.querySelector("#compScore")
+
+
+function playGame() {
+    rock.addEventListener("click", () => {
+        playRound("rock", getComputerChoice())
+    })
+    paper.addEventListener("click", () => {
+        playRound("paper", getComputerChoice())
+    })
+    scissors.addEventListener("click", () => {
+        playRound("scissors", getComputerChoice())
+    })
 }
+
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice){
-        console.log("Tie: You both choose " + humanChoice)
-    }
-    else if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissor") 
-        || (humanChoice === "scissor" && computerChoice === "rock") ){
-        
-        console.log(`You Lose: ${computerChoice} beats ${humanChoice}`)
-        computerScore += 1
-    }
-    else{
-        console.log(`You Win: ${humanChoice} beats ${computerChoice}`)
-        humanScore += 1
-    }
-}
-const computerSelection = getComputerChoice
-const humanSelection = getHumanChoice
 
-function playGame() {
-    for (let i = 0; i < 5; i++){
-        playRound(humanSelection(), computerSelection())
+    if (humanChoice === computerChoice) {
+        currentResult.textContent = "Tie: You both choose " + humanChoice
+        results.appendChild(currentResult)
     }
-    console.log((humanScore > computerScore) ? `You Won! with the total score of ${humanScore}` : `Computer Won! with the total 
-    score of ${computerScore}`)
+    else if ((humanChoice === "rock" && computerChoice === "paper") || (humanChoice === "paper" && computerChoice === "scissors") || (humanChoice === "scissors" && computerChoice === "rock")) {
+
+        currentResult.textContent = `You Lose: ${computerChoice} beats ${humanChoice}`
+        computerScore += 1
+        cScore.textContent = computerScore
+    }
+    else {
+        currentResult.textContent = `You Win: ${humanChoice} beats ${computerChoice}`
+        humanScore += 1
+        hScore.textContent = humanScore
+    }
+    if (humanScore == 5 || computerScore == 5) {
+        finalResult.textContent = (humanScore > computerScore) ? `You Won! by ${humanScore - computerScore} point(s)`
+            : `Computer Won! by ${computerScore - humanScore} point(s)`
+        
+        hScore.textContent = 0
+        cScore.textContent = 0
+        
+        humanScore = 0
+        computerScore = 0
+    }
+
 }
+
 playGame()
